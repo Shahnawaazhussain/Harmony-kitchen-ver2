452b3b3470abd076738f61a2991b3a8b2d04f02f
Shahnawaazhussain <77885314+Shahnawaazhussain@users.noreply.github.com>
2023-09-16 20:36:52 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $datetime = $_POST['datetime'];
+    $people = $_POST['people'];
+    $msg = $_POST['msg'];
+
+    //Database Connection
+    
+?>
